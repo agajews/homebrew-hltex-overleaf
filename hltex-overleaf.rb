4c3e8aff1af6ac7eef51c998eba66770227ebd4c
Alex Gajewski <agajews@gmail.com>
2019-01-10 13:02:09 -0800

    fixed syntax error

--- hltex-overleaf.rb
+++ hltex-overleaf.rb
@@ -1,14 +1,12 @@
 require 'fileutils'
 
 
-class HLTeXOverleaf < Formula
+class HltexOverleaf < Formula
   desc "Extras for a Chrome extension integrating HLTeX with Overleaf"
-  homepage "https://github.com/agajews/hltex-chrome"
-  url "https://github.com/agajews/hltex-chrome/raw/master/extras.tar.gz"
+  homepage "https://github.com/agajews/homebrew-hltex-overleaf"
+  url "https://github.com/agajews/homebrew-hltex-overleaf/raw/master/extras.tar.gz"
   sha256 "422ccf5e0ef6aad32a86a2a5616f11992193104a8a7a13c31daba30d995844f2"
   version "0.0.1"
-
-  depends_on "curl"
 
   bottle :unneeded
 
