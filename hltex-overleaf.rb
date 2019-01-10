9ac0461ff0552be367b91024a3379c2c0c7cb3eb
Alex Gajewski <agajews@gmail.com>
2019-01-10 13:04:26 -0800

    fixed typo

--- hltex-overleaf.rb
+++ hltex-overleaf.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 class HltexOverleaf < Formula
   desc "Extras for a Chrome extension integrating HLTeX with Overleaf"
   homepage "https://github.com/agajews/homebrew-hltex-overleaf"
-  url "https://github.com/agajews/homebrew-hltex-overleaf/raw/master/extras.tar.gz"
+  url "https://github.com/agajews/homebrew-hltex-overleaf/raw/master/extras-0.0.1.tar.gz"
   sha256 "422ccf5e0ef6aad32a86a2a5616f11992193104a8a7a13c31daba30d995844f2"
   version "0.0.1"
 
