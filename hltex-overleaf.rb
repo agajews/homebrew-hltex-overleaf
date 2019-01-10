8caa0dd382bfca82dafe050086540cd4a3676c4b
Alex Gajewski <agajews@gmail.com>
2019-01-10 13:13:42 -0800

    fixed typo

--- hltex-overleaf.rb
+++ hltex-overleaf.rb
@@ -2,18 +2,18 @@ require 'fileutils'
 
 
 class HltexOverleaf < Formula
-  desc "Extras for a Chrome extension integrating HLTeX with Overleaf"
-  homepage "https://github.com/agajews/homebrew-hltex-overleaf"
-  url "https://github.com/agajews/homebrew-hltex-overleaf/raw/master/extras-0.0.1.tar.gz"
-  sha256 "422ccf5e0ef6aad32a86a2a5616f11992193104a8a7a13c31daba30d995844f2"
-  version "0.0.1"
+    desc "Extras for a Chrome extension integrating HLTeX with Overleaf"
+    homepage "https://github.com/agajews/homebrew-hltex-overleaf"
+    url "https://github.com/agajews/homebrew-hltex-overleaf/raw/master/extras-0.0.1.tar.gz"
+    sha256 "422ccf5e0ef6aad32a86a2a5616f11992193104a8a7a13c31daba30d995844f2"
+    version "0.0.1"
 
-  bottle :unneeded
+    bottle :unneeded
 
-  def install
-      FileUtils.mkdir_p  "/Library/Google/Chrome/NativeMessagingHosts/com.hltex.overleaf.json"
-      FileUtils.cp_r "com.hltex.overleaf.json", "/Library/Google/Chrome/NativeMessagingHosts/com.hltex.overleaf.json", remove_destination: true
-      FileUtils.mkdir_p  "/usr/local/bin"
-      FileUtils.cp_r "overleaf_translator", "/usr/local/bin/", remove_destination: true
-  end
+    def install
+        FileUtils.mkdir_p  "/Library/Google/Chrome/NativeMessagingHosts"
+        FileUtils.cp_r "com.hltex.overleaf.json", "/Library/Google/Chrome/NativeMessagingHosts/com.hltex.overleaf.json", remove_destination: true
+        FileUtils.mkdir_p  "/usr/local/bin"
+        FileUtils.cp_r "overleaf_translator", "/usr/local/bin/", remove_destination: true
+    end
 end
